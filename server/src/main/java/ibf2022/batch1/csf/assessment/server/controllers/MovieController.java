ee1d42a9a3acefe295813b254d5920f2560f7ee3
Wee Seng <wee.bscl@gmail.com>
2023-04-05 10:20:48 +0800

    getsvc

--- server/src/main/java/ibf2022/batch1/csf/assessment/server/controllers/MovieController.java
+++ server/src/main/java/ibf2022/batch1/csf/assessment/server/controllers/MovieController.java
@@ -1,5 +1,7 @@
 package ibf2022.batch1.csf.assessment.server.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ibf2022.batch1.csf.assessment.server.models.Review;
 import ibf2022.batch1.csf.assessment.server.services.MovieService;
 import jakarta.json.Json;
 
@@ -24,7 +27,7 @@ public class MovieController {
 	@GetMapping(path = "/search")
 	public ResponseEntity<String> Task3SearchReview(@RequestParam String query) {
 
-		// movieSvc.
+		List<Review> reviewList = movieSvc.searchReviews(query);
 
 		String response = Json.createObjectBuilder()
 				.add("test", "HEY WASSAP")
